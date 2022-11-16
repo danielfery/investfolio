812aec3ed6cb488cf2f3343abee43b4b7a91fd40
danielfery <daniel.fery-enzlmueller@gmx.at>
2022-11-16 08:41:18 +0100

    Zwischenschritt: In Postman ein POST an http://localhost:8080/api/stock erfolgreich

--- investfolio/src/main/java/com/example/investfolio/entity/Stock.java
+++ investfolio/src/main/java/com/example/investfolio/entity/Stock.java
@@ -2,6 +2,12 @@ package com.example.investfolio.entity;
 
 public class Stock{
     private String ticker;
+
+/*    public Stock(String ticker, String companyName) {
+        this.ticker = ticker;
+        this.companyName = companyName;
+    }*/
+
     private String companyName;
 
     public String getCompanyName() {
